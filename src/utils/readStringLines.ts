520a71ff71bb5b90a24997babd651fc8925dd734
Fynn Haupt <info@fynnhaupt.de>
2024-10-15 14:01:41 +0200

    dont filter out empty lines

--- src/utils/readStringLines.ts
+++ src/utils/readStringLines.ts
@@ -1,6 +1,3 @@
 export function readStringLines(buffer: Buffer) {
-  return buffer
-    .toString("utf8")
-    .split("\n")
-    .filter((n) => n.length > 0);
+  return buffer.toString("utf8").split("\n");
 }

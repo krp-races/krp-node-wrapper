bda1138819ca1bbab37a3f7387aba125e29dbd1a
Fynn Haupt <info@fynnhaupt.de>
2024-10-20 14:11:57 +0200

    added test script for local testing

--- test.js
+++ test.js
@@ -5,12 +5,27 @@ const client = new Client({
   host: "127.0.0.1",
   port: 5100,
   password: "123",
-  trackPositions: 1,
-  collisions: 2,
+  trackPositions: 0,
+  collisions: 0,
 });
 
+client.on("connected", () => console.log("Connected Livetiming Client"));
+client.on("disconnected", () => console.log("Disconnected Livetiming Client"));
+//client.on("data", (data) => console.log(data));
+client.on("error", (err) => console.error(err));
+
+console.log("Enable Livetiming Client");
 client.setEnabled(true);
 
-client.on("connected", () => console.log("connected"));
-client.on("disconnected", () => console.log("disconnected"));
-client.on("error", (err) => console.error(err));
+process.on("SIGINT", function () {
+  console.log("Disable Livetiming Client");
+
+  if (client.getStatus() === 2) {
+    client.once("disconnected", () => process.exit(0));
+    setTimeout(() => process.exit(1), 2000);
+    client.setEnabled(false);
+  } else {
+    client.setEnabled(false);
+    process.exit(0);
+  }
+});

c042236c3a92b366359f320da8b10ce238d49a53
Fynn Haupt <info@fynnhaupt.de>
2024-11-30 00:07:13 +0100

    export live timing data interface

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import { RemoteAdminClient } from "./modules/RemoteAdminClient";
 
 export { LivetimingClient } from "./modules/LivetimingClient";
 export { RemoteAdminClient } from "./modules/RemoteAdminClient";
+export { LiveTimingData } from "./interfaces/LiveTimingData";
 export default { LivetimingClient, RemoteAdminClient };
